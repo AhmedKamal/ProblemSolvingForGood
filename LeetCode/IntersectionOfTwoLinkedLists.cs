c8ba3f6bcc31a223ff364f846404ca9e1b1f96f0
Ahmed Kamal <a.abdelfatah@live.com>
2015-05-20 16:31:28 +0300

    add LeetCode

---
+++ LeetCode/IntersectionOfTwoLinkedLists.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+
+    public class  IntersectionOfTwoLinkedLists{
+        static Dictionary<int, short> dic;
+
+        public static ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+            dic = new Dictionary<int, short>();
+            TraverseLL(headA);
+            TraverseLL(headB);
+
+            ListNode intersect = null;
+            bool found = false;
+            while (headA != null)
+            {
+                if (dic[headA.val] == 2 & !found)
+                {
+                    intersect = headA;
+                    found = true;
+                }
+                if (dic[headA.val] == 1)
+                    found = false;
+
+                headA = headA.next;
+            }
+
+            if (!found) return null;
+            return intersect;
+        }
+
+        public static void TraverseLL(ListNode n)
+        {
+            ListNode head = n;
+            while (head != null)
+            {
+                if (dic.ContainsKey(head.val))
+                    dic[head.val]++;
+
+                else
+                    dic.Add(head.val, 1);
+                head = head.next;
+            }
+        }
+
+
+    }
+}
---
+++ LeetCode/ListNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+
+      public class ListNode {
+      public int val;
+      public ListNode next;
+      public ListNode(int x) { val = x; }
+  
+    }
+}
